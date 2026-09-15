7ed666921acc7f2b27a988dea5b5f956a912364a
s2h2sr2 <gorantsa@katharineum.de>
2026-09-15 13:46:07 +0200

    Load TensorFlow model in app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from PIL import Image
 import tensorflow as tf
 import os
 from datetime import date
-
+import tf_keras as tf
+model = tf.keras.models.load_model(MODEL_PATH)
 # ─────────────────────────────────────────
 # Seitenkonfiguration
 # ─────────────────────────────────────────
